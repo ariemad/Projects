4d817ba840a857b10a9f325b2a0ddc6587bb0b92
Daniel Bray <daniel.bray.dev@hotmail.com>
2022-09-19 15:03:20 +0100

    Operators and calculate

---
+++ Calculator/js-file.js
@@ -0,0 +1,18 @@
+//Solve example subtract(0.1 , 0.4) = -0.300000000000000000004
+
+function add(a,b) {
+    return a+b
+}
+function subtract(a,b) {
+    return a-b
+}
+function multiply(a,b) {
+    return a*b
+}
+function divide(a,b) {
+    return a/b
+}
+
+function calculate(operator,a,b) {
+    return operator(a,b)
+}
