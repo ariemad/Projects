b331443f42fd894b4a2583e4abd40d8faa7f56f9
Daniel Bray <daniel.bray.dev@hotmail.com>
2022-09-20 11:30:18 +0100

    Add divideByZero, percentage, equal spam fix

--- Calculator/js-file.js
+++ Calculator/js-file.js
@@ -2,6 +2,10 @@
 
 // Arithmetical
 
+function round3Decimals(num) {
+  return Math.round((num + Number.EPSILON) * 1000) / 1000;
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -12,7 +16,11 @@ function multiply(a, b) {
   return a * b;
 }
 function divide(a, b) {
-  return a / b;
+  if (b == "0") {
+    divideByZero();
+    return "div0error";
+  }
+  return round3Decimals(a / b);
 }
 
 function calculate(operator, a, b) {
@@ -23,6 +31,7 @@ function calculate(operator, a, b) {
 
 function displayNum() {
   display.textContent = memDisplay;
+  overloadCheck()
 }
 
 function displayOperator(e) {
@@ -31,12 +40,47 @@ function displayOperator(e) {
 
 function overloadCheck() {
   if (memDisplay.length > 8) {
-    firstNumber = 0;
-    memOperator = "";
     memDisplay = "Overload";
     displayNum();
+    setTimeout(() => {
+      firstNumber = 0;
+      secondNumber = "";
+      memOperator = "";
+      memDisplay = "";
+      displayNum();
+    }, 2000);
   }
 }
+
+function divideByZero() {
+  memDisplay = "ERROR";
+  displayNum();
+  setTimeout(() => {
+    memDisplay = "DIV 0";
+    displayNum();
+  }, 1000);
+  setTimeout(() => {
+    firstNumber = 0;
+    secondNumber = "";
+    memOperator = "";
+    memDisplay = "";
+    displayNum();
+  }, 2000);
+}
+
+//Flag
+
+function isLastNumber(memDisplay) {
+  lastDigit = memDisplay[memDisplay.length - 1];
+  digits = "0123456789".split("");
+  if (lastDigit in digits) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Main
 
 firstNumber = 0;
 secondNumber = "";
@@ -49,52 +93,70 @@ const btnsNum = document.querySelectorAll("[data-num]");
 
 const btnClear = document.querySelector("#clear");
 
+const btnPercentage = document.querySelector("#percentage")
+
 const btnsOperation = document.querySelectorAll("[data-operation]");
 
 btnsNum.forEach((element) => {
   element.addEventListener("click", (e) => {
+    if (memOperator === "" && firstNumber) {
+      firstNumber = 0;
+      secondNumber = "";
+      memDisplay = "";
+    }
     secondNumber += e.target.textContent;
     memDisplay += e.target.textContent;
-    overloadCheck();
     displayNum();
   });
 });
 
 btnsOperation.forEach((element) => {
   element.addEventListener("click", (e) => {
-    if (memOperator) {
-      functionName = eval(memOperator);
-      firstNumber = calculate(
-        functionName,
-        Number(firstNumber),
-        Number(secondNumber)
-      );
-      
-      switch (e.target.getAttribute("data-operation")) {
-        case "equal":
-          secondNumber = firstNumber;
-          memOperator = "";
-          memDisplay = String(firstNumber);
-          break;
-        default:
-          secondNumber = "";
-          memOperator = e.target.getAttribute("data-operation");
-          memDisplay =
-            String(firstNumber) + e.target.textContent;
+    breakme: {
+      if (memOperator && secondNumber !== "") {
+        functionName = eval(memOperator);
+        firstNumber = calculate(
+          functionName,
+          Number(firstNumber),
+          Number(secondNumber)
+        );
+        if (firstNumber === "div0error") {
+          break breakme;
+        }
+        switch (e.target.getAttribute("data-operation")) {
+          case "equal":
+            secondNumber = firstNumber;
+            memOperator = "";
+            memDisplay = String(firstNumber);
+            break;
+          default:
+            secondNumber = "";
+            memOperator = e.target.getAttribute("data-operation");
+            memDisplay = String(firstNumber) + e.target.textContent;
+        }
+      } else if (memOperator && secondNumber === "") {
+        switch (e.target.getAttribute("data-operation")) {
+          case "equal":
+            break;
+          default:
+            memOperator = e.target.getAttribute("data-operation");
+            memDisplay = memDisplay.slice(0, -1);
+            memDisplay += e.target.textContent;
+        }
+      } else {
+        // !memOperator
+        switch (e.target.getAttribute("data-operation")) {
+          case "equal":
+            break;
+          default:
+            firstNumber = secondNumber;
+            secondNumber = "";
+            memOperator = e.target.getAttribute("data-operation");
+            memDisplay += e.target.textContent;
+        }
       }
-    } else {
-      switch (e.target.getAttribute("data-operation")) {
-        case "equal":
-          break;
-        default:
-          firstNumber = secondNumber;
-          secondNumber = "";
-          memOperator = e.target.getAttribute("data-operation");
-          memDisplay += e.target.textContent;
-      }
+      displayNum();
     }
-    displayNum();
-    overloadCheck();
   });
 });
 
@@ -105,3 +167,19 @@ btnClear.addEventListener("click", (e) => {
   memDisplay = "";
   displayNum();
 });
+
+btnPercentage.addEventListener("click", e => {
+  if (secondNumber !== "" && isLastNumber(memDisplay)) {
+    if (memOperator) {
+      memDisplay = memDisplay.slice(0,-secondNumber.length)
+      secondNumber = secondNumber/100
+      memDisplay += secondNumber
+      displayNum()
+    } else {
+      secondNumber = secondNumber/100
+      memDisplay = secondNumber
+      displayNum()
+    }
+  }
+
+})
