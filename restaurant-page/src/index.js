3dc7906efa73b08a8aae586bda3d89259161c5fc
Daniel Bray <daniel.bray.dev@hotmail.com>
2022-10-03 16:03:20 +0100

    Deleted and created Webpack.config and package.json

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,1 +1,27 @@
+import "./style.scss"
+import _ from 'lodash';
+import MyImage from "./img/fish.jpg"
+
+function component() {
+    const element = document.createElement('div');
+  
+  // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+
 alert("Hey you")
+  
+  const heroContentEL = document.createElement("div")
+  heroContentEL.classList.add("hero")
+  heroContentEL.textContent="Hello im heroContent"
+  
+  const imgFish = new Image()
+  imgFish.src = MyImage
+  
+  heroContentEL.appendChild(imgFish)
+  
+  document.body.append(heroContentEL)
--- restaurant-page/webpack.config.js
+++ restaurant-page/webpack.config.js
@@ -1,9 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
